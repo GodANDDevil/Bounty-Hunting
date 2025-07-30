3fbf1469fd9e853110e61e7b58f0a652069090d7
GodANDDevil <anamolshr1234@gmail.com>
2025-07-30 14:03:09 +0545

    #

--- BountyHunting/models.py
+++ BountyHunting/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Scout(models.Model):
-    scout_name = models.CharField(max_length=100)
+    scout_Company_name = models.CharField(max_length=100)
     scout_gmail = models.EmailField()
     scout_password = models.CharField(max_length=100)
     scout_phone = models.CharField(max_length=15)
@@ -11,7 +11,7 @@ class Scout(models.Model):
     scout_reg_no = models.CharField(max_length=50)
 
 class Seeker(models.Model):
-    seeker_name = models.CharField(max_length=100)
+    seeker_Full_name = models.CharField(max_length=100)
     seeker_gmail = models.EmailField()
     seeker_password = models.CharField(max_length=100)
     seeker_phone = models.CharField(max_length=15)
