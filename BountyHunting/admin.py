63180969b864c5c1adb53cc9628d7b1590a6b96b
GodANDDevil <anamolshr1234@gmail.com>
2025-09-01 21:45:01 +0545

    #

--- BountyHunting/admin.py
+++ BountyHunting/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
-# Register your models here.
+from .models import Scout, Seeker, JobPost
+
+admin.site.register(Scout)
+admin.site.register(Seeker)
+admin.site.register(JobPost)
